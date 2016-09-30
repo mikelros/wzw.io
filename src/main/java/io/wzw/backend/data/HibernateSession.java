5218134f298282023463428593fd3f7f067f1e9a
DavoLarris <davo.larris@hotmail.com>
2016-09-30 13:31:57 +0200

    To Asier

---
+++ src/main/java/io/wzw/backend/data/HibernateSession.java
@@ -0,0 +1,5 @@
+package io.wzw.backend.data;
+
+public class HibernateSession {
+
+}
--- src/main/java/io/wzw/backend/data/Main.java
+++ src/main/java/io/wzw/backend/data/Main.java
@@ -4,7 +4,7 @@ package io.wzw.backend.data;
  * Hello world!
  *
  */
-public class App 
+public class Main 
 {
     public static void main( String[] args )
     {
