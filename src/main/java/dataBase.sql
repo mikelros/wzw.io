322771c6e821d840b018da37f14150076cb0f971
DavoLarris <davo.larris@hotmail.com>
2016-10-03 14:15:56 +0200

    sql added

---
+++ src/main/java/dataBase.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2016 a las 14:14:37
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `wzwdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `userrole`
+--
+
+CREATE TABLE `userrole` (
+  `idauto` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `idrole` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `userrole`
+--
+ALTER TABLE `userrole`
+  ADD PRIMARY KEY (`idauto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `userrole`
+--
+ALTER TABLE `userrole`
+  MODIFY `idauto` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/test/java/io/wzw/backend/data/dao/impl/HibernateRoleDAOTest.java
@@ -0,0 +1,105 @@
+package io.wzw.backend.data.dao.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.wzw.backend.data.dao.RoleDAO;
+import io.wzw.backend.data.dao.impl.HibernateRoleDAO;
+import io.wzw.backend.data.model.Role;
+
+public class HibernateRoleDAOTest {
+
+	private RoleDAO roleDAO;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		roleDAO = new HibernateRoleDAO();
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testSelectById() {
+		// Select after insert
+		Role insertRole = new Role(null,"Admin","Administrator role");
+		roleDAO.insert(insertRole);
+		Role role = roleDAO.selectById(insertRole.getId());
+		assertEquals("Select by Id should exist",role.getId(), insertRole.getId());
+		
+	}
+
+	/**
+	 * Test method for {@link io.wzw.backend.data.dao.impl.HibernateRoleDAO#selectAll()}.
+	 */
+	@Test
+	public void testSelectAll() {
+		int totalElements = roleDAO.selectAll().size();
+		
+		Role insertRole = new Role(null,"Admin","Administrator role");
+		roleDAO.insert(insertRole);
+		int totalElementsAfterInsert = roleDAO.selectAll().size();
+		
+		assertEquals("Select All returns all elements",totalElements + 1, totalElementsAfterInsert);
+	}
+
+	/**
+	 * Test method for {@link io.wzw.backend.data.dao.impl.HibernateRoleDAO#insert(io.wzw.backend.data.model.Role)}.
+	 */
+	@Test
+	public void testInsert() {
+		assertTrue("Insert",true);
+	}
+
+	/**
+	 * Test method for {@link io.wzw.backend.data.dao.impl.HibernateRoleDAO#update(io.wzw.backend.data.model.Role)}.
+	 */
+	@Test
+	public void testUpdate() {
+		String updatedName = "Admin changed";
+		
+		// Select after first insert
+		Role insertRole = new Role(null,"Admin","Administrator role");
+		roleDAO.insert(insertRole);
+		
+		// We update the role
+		insertRole.setName(updatedName);
+		roleDAO.update(insertRole);
+		
+		// Select and check if name has changed
+		Role updatedRole = roleDAO.selectById(insertRole.getId());
+		
+		assertEquals("Role name was changed", updatedName, updatedRole.getName());
+	}
+
+	/**
+	 * Test method for {@link io.wzw.backend.data.dao.impl.HibernateRoleDAO#delete(io.wzw.backend.data.model.Role)}.
+	 */
+	@Test
+	public void testDelete() {
+		// Select after first insert
+		Role insertRole = new Role(null,"Admin","Administrator role");
+		roleDAO.insert(insertRole);
+		
+		// Delete 
+		roleDAO.delete(insertRole);
+		Role role = roleDAO.selectById(insertRole.getId());
+		assertNull("Select by Id with a deleted record id shoud be null",role);
+		
+	}
+
+}
