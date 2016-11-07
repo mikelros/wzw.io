66607b8663a6b1e0b93d7ccb91f8d2e3f4fd1fab
Asgarth <Asgarth@172.30.0.40>
2016-11-07 14:13:09 +0100

    meetup corrección

--- src/main/java/io/wzw/backend/data/dao/impl/HibernateMeetupDAO.java
+++ src/main/java/io/wzw/backend/data/dao/impl/HibernateMeetupDAO.java
@@ -10,5 +10,34 @@ import io.wzw.backend.data.dao.MeetupDAO;
 import io.wzw.backend.data.model.Meetup;
 
 public class HibernateMeetupDAO extends GenericDAOHibernate<Meetup> implements MeetupDAO{
-	
+	public void insert(Meetup meetup) {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Integer id = (Integer) session.save(meetup);
+		meetup.setId(id);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
+
+	public void update(Meetup meetup) {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.merge(meetup);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
+
+	public void delete(Meetup meetup) {
+		SessionFactory sessionFactory = HibernateSession.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.delete(meetup);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
 }
